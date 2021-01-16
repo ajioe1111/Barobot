ab86f3c4bf8608d76d5e6ed2365d9022f4c52e84
Fuuka <ajioe1111@gmail.com>
2021-01-16 23:34:05 +0300

    Обновление чекера ссылок.

--- service/urlChecker.js
+++ service/urlChecker.js
@@ -6,12 +6,16 @@ import * as Discord from 'discord.js';
  * @param {Discord.} msg 
  */
 
-export function urlChecker (msg, isAdmin) {
-    if (isAdmin) {
+export function urlChecker (msg, isAdmin, isModer) {
+    if (isAdmin || isModer || msg.author.bot) {
         return;
     }
+    if (msg.content.startsWith(`https://discord.com/channels/@me`)) {
+        return;
+    }
+
     let lowerContent = msg.content.toLowerCase();
-    let url = ['http', 'https', '.www', '://', '.ru', '.com', '.net'];
+    let url = ['http', 'https', '.www', '://', '.ru', '.com', '.net', '.info'];
     for (let i = 0; i <= url.length; i++) {
         if (lowerContent.includes(url[i])) {
             msg.delete( {timeout: 0});
