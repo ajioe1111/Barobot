ea01efbceccdc3bd142dd830ee6b74929d0e69cb
Fuuka <ajioe1111@gmail.com>
2021-01-19 22:03:40 +0300

    fix

--- service/urlChecker.js
+++ service/urlChecker.js
@@ -17,12 +17,12 @@ export function urlChecker(msg, isAdmin, isModer, isOwner, logChannel) {
         if (msg.content === `$$checkurl true` && isAdmin || msg.content === `$$checkurl true` && isOwner) {
             options.urlCheck = true;
             msg.reply(`Запрет ссылок включен.`);
-            fs.writeFileSync(`./configs/options.json`, JSON.stringify(options));
+            fs.writeFileSync(`./database/guild/${guildID}.json`, JSON.stringify(options));
             return;
         } else if (msg.content === `$$checkurl false` && isAdmin || msg.content === `$$checkurl false` && isOwner) {
             options.urlCheck = false;
             msg.reply(`Запрет ссылок выключен.`);
-            fs.writeFileSync(`./configs/options.json`, JSON.stringify(options));
+            fs.writeFileSync(`./database/guild/${guildID}.json`, JSON.stringify(options));
             return;
         }
 
