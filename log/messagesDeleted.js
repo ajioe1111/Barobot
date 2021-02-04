11a3516dc72dcf6daf642ec29d80974fce3f8b97
Fuuka <ajioe1111@gmail.com>
2021-02-04 23:05:21 +0300

    fix bug

--- log/messagesDeleted.js
+++ log/messagesDeleted.js
@@ -1,14 +1,16 @@
 import * as Discord from 'discord.js';
 
 export function dellMsg(messageDelete, logChannel) {
-    console.log(`${messageDelete.author} delete msg!`)
-    const DeleteEmbed = new Discord.MessageEmbed()
-    .setTitle("**Удалено сообщение!**")
-    .setColor("#fc3c3c")
-    .addField("Автор", `<@${messageDelete.author.id}>`, true)
-    .addField("Канал", messageDelete.channel, true)
-    .addField("Сообщение", messageDelete.content)
-    .setFooter(`ID Сообщения: ${messageDelete.id} | ID Автора: ${messageDelete.author.id}`);
-  
-    logChannel.send(DeleteEmbed);
+    if (messageDelete !== undefined) {
+        console.log(`${messageDelete.author} delete msg!`)
+        const DeleteEmbed = new Discord.MessageEmbed()
+            .setTitle("**Удалено сообщение!**")
+            .setColor("#fc3c3c")
+            .addField("Автор", `<@${messageDelete.author.id}>`, true)
+            .addField("Канал", messageDelete.channel, true)
+            .addField("Сообщение", messageDelete.content)
+            .setFooter(`ID Сообщения: ${messageDelete.id} | ID Автора: ${messageDelete.author.id}`);
+
+        logChannel.send(DeleteEmbed);
+    }
 }
