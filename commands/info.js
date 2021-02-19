1c31a219a13f9e0e09905472139a51f5022ffaef
Fuuka <ajioe1111@gmail.com>
2021-02-19 14:26:07 +0300

    fix info

--- commands/info.js
+++ commands/info.js
@@ -1,8 +1,12 @@
 import * as Discord from 'discord.js';
+import * as fs from 'fs';
 
 export function info(msg, botlog) {
     let user = msg.mentions.users.first();
     if (user) {
+        let findUser = (`./database/users/${user.id}.json`);
+        let cacheUser = fs.readFileSync(findUser).toString();
+        let userProperties = JSON.parse(cacheUser);
         let userID = user.id;
         const embed = new Discord.MessageEmbed()
             .setImage(user.displayAvatarURL())
@@ -11,8 +15,8 @@ export function info(msg, botlog) {
             .addField(`ID:`, `<@${userID}> ${userID}`)
             .addField(`Регистрация:`, `${user.createdAt}`)
             .addField(`ТЭГ:`, `${user.tag}`)
-            .addField(`Кол-во предупреждений`, `${user.warnCount}`)
-            .addField(`Кол-во банов за предупреждения`, `${user.warn_end}`)
+            .addField(`Кол-во предупреждений`, `${userProperties.warnCount}`)
+            .addField(`Кол-во банов за предупреждения`, `${userProperties.warn_end}`)
             .setColor(0xff0000);
         botlog.send(embed);
     } else { botlog.send(`Текущего юзера нету на сервере, воспользуйтесь командой !infoID или запросите базу данных`) }
